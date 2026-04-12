8c7c38f335d9dc3b771aa4f4c1bb8aa30f353b57
TMname1 <tmname0@gmail.com>
2026-04-12 18:56:25 +0800

    feat: initialize project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const a = 123;
+console.log(a);
+const b = 333;
+console.log(b);
+</script>
+
+<template>
+  <h1>你好</h1>
+  <button class="m-1 bg-sky-700 px-4 py-3 text-white">click</button>
+</template>
